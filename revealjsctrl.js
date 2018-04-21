df9426df52e452a91c2b5c6edee4e0965d7b0b88
Chithra Boopalan <chithraboopalan@gmail.com>
2018-04-22 01:20:04 +0530

    Add revealjs control function stubs

---
+++ revealjsctrl.js
@@ -0,0 +1,32 @@
+
+
+function getNumberOfMainSlides() {
+  let mainSlides = $('div.reveal-frame div.slides > section');
+  return mainSlides.length;
+}
+
+function getNumberOfSubSlides(mainSlide) {
+
+}
+
+function getCurrentMainSlide() {
+
+}
+
+function getCurrentSubSlide(mainSlide) {
+
+}
+
+function goToSlide(mainSlide, subSlide) {
+
+}
+
+function goToNextSlide() {
+
+}
+
+function goToPreviousSlide() {
+
+}
+
+console.log(getNumberOfMainSlides());
