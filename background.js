71fd48551f558b9c8013fc43ef203fbeb9041607
Chithra Boopalan <chithraboopalan@gmail.com>
2018-04-22 01:21:10 +0530

    Add programmatic declarative page action
    This will enable the page action only on pages that have a revealjs
    presentation.

--- background.js
+++ background.js
@@ -1,14 +1,41 @@
+var rule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      css: ["div.reveal"]
+    })
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+};
+
 chrome.runtime.onInstalled.addListener(function() {
-   chrome.storage.sync.set({color: '#3aa757'}, function() {
-     console.log("The color is green.");
-   });
-   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
+  });
+  chrome.storage.sync.set({
+    color: '#3aa757'
+  }, function() {
+    console.log("The color is green.");
+  });
+
+  chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+      file: 'jquery-3.3.1.js'
+    });
+    chrome.tabs.executeScript({
+      file: 'revealjsctrl.js'
+    });
+  });
+
+  /*
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+          pageUrl: {
+            hostEquals: 'revealjs.com'
+          },
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
- });
+  */
+});
