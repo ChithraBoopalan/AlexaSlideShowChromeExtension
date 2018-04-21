a605a8fff9766c72e278536647a61c54534c170c
Chithra Boopalan <chithraboopalan@gmail.com>
2018-04-22 01:22:22 +0530

    Setup basic websocket connection
    This connects to the websocket test server and goes the next slide on received
    a "next" message over websocket.

--- popup.js
+++ popup.js
@@ -1,19 +1,41 @@
 let changeColor = document.getElementById('changeColor');
+let connectToWebsocket = document.getElementById('connectWebSocket');
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id, {
+        code: 'console.log(document.getElementsByClassName("navigate-right")[0].click())'
+      });
   });
+};
 
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({
-              active: true,
-              currentWindow: true
-          }, function(tabs) {
-            console.log(tabs);
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-        });
-  };
+connectToWebsocket.onclick = function(element) {
+  var socket = new WebSocket("ws://localhost:8000");
+  socket.onopen = function(event) {
+    socket.send("Hi");
+  }
+
+  socket.onmessage = function(event) {
+    if(event.data == "next") {
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, function(tabs) {
+        chrome.tabs.executeScript(
+          tabs[0].id, {
+            code: 'console.log(document.getElementsByClassName("navigate-right")[0].click())'
+          });
+      });
+    }
+  }
+}
