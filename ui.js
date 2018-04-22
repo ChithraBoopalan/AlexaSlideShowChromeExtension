be0d00c3bf785ce8e3516d57c87b4f62d16a79c2
Kumar S <skmrx@opmbx.org>
2018-04-22 20:14:07 +0530

    Add ui.js with modals that we want to show

---
+++ ui.js
@@ -0,0 +1,34 @@
+function showConnectionId(id) {
+  // TODO close the popup when Alexa device connects
+
+  let m = $('<div title="Alexa Slide Show">' +
+    'This is your slide show code:<br />' +
+    '<h1 style="text-align: center">' + id + '</h1><br />' +
+    'Use any Alexa device to control this presentation by saying:' +
+    '<pre>Alexa, ask Slide Show to start slide ' + id + '</pre>' +
+    '</div>');
+  m.dialog({
+    modal: true,
+    buttons: {
+      Ok: function() {
+        $(this).dialog("close");
+      }
+    }
+  });
+}
+
+function showDisconnected() {
+  let m = $('<div title="Alexa Slide Show">' +
+    "Oops, you've been disconnected from Alexa." +
+    "Please reload the page and try connecting again." +
+    '</div>');
+  m.dialog({
+    modal: true,
+    buttons: {
+      Ok: function() {
+        $(this).dialog("close");
+      }
+    }
+  });
+}
+
