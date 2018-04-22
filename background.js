70789c9211904fe8e59e1d1f83752a225d3aad8f
Chithra Boopalan <chithraboopalan@gmail.com>
2018-04-22 10:23:19 +0530

    Remove references to popup and options
    We don't need them anymore.

--- background.js
+++ background.js
@@ -11,11 +11,6 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([rule]);
   });
-  chrome.storage.sync.set({
-    color: '#3aa757'
-  }, function() {
-    console.log("The color is green.");
-  });
 
   chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({
@@ -40,17 +35,4 @@ chrome.runtime.onInstalled.addListener(function() {
       }
     });
   });
-
-  /*
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {
-            hostEquals: 'revealjs.com'
-          },
-        })],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  */
 });
