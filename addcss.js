97815a20e801afd1d8cb634aab9cbde801d9a7cf
Kumar S <skmrx@opmbx.org>
2018-04-22 20:14:50 +0530

    addcss.js: Add CSS for jquery ui

--- addcss.js
+++ addcss.js
@@ -1,2 +1,11 @@
+// This didn't work and Chrome shows the error like chrome://invalid/ net::ERR_FAILED
+/*
 // Add the jQuery Modal CSS to the page
-$('<link rel="stylesheet" type="text/css" href="' + chrome.extension.getURL("lib/jquery.modal.css") + '">').appendTo("head");
+let jqueryModalCss = chrome.extension.getURL('lib/jquery.modal.css');
+
+$('<link rel="stylesheet" href="' + jqueryModalCss + '" />').appendTo("head");
+*/
+//$('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />').appendTo('head');
+
+$('<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />').appendTo('head');
+
