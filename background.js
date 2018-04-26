6026072aea28a1c70b880cc2ff981ad21fa951ce
Chithra Boopalan <chithraboopalan@gmail.com>
2018-04-26 23:36:12 +0530

    Move pageAction.onClicked listener to top level
    There were cases where the page action click event was not being generated.
    This may fix that.

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 var rule = {
+  id: 'showPageAction',
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
       css: ["div.reveal"]
@@ -8,41 +9,46 @@ var rule = {
 };
 
 chrome.runtime.onInstalled.addListener(function() {
+  console.log("onInstalled Listener added");
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([rule]);
+    console.log("onPageChanged rules added");
   });
+});
 
-  chrome.pageAction.onClicked.addListener(function(tab) {
+chrome.pageAction.onClicked.addListener(function(tab) {
+  console.log("onClicked listener triggered");
+  chrome.tabs.executeScript({
+    // Check if the revealjsInjected variable has been defined
+    code: 'typeof revealjsInjected === "undefined"'
+  }, function(results) {
     chrome.tabs.executeScript({
-      // Check if the revealjsInjected variable has been defined
-      code: 'typeof revealjsInjected === "undefined"'
-    }, function(results) {
+      code: 'console.log("Alexa Slide Show needs injection? ' + results[0] + '")'
+    });
+
+    if (results[0]) {
+      // Define the revealjsInjected variable so that it can be used to
+      // ensure that scripts are injected only once
       chrome.tabs.executeScript({
-        code: 'console.log("Alexa Slide Show needs injection? ' + results[0] + '")'
+        code: 'var revealjsInjected = true;'
       });
 
-      if (results[0]) {
-        // Define the revealjsInjected variable so that it can be used to
-        // ensure that scripts are injected only once
+      let jsFiles = [
+        'lib/jquery-3.3.1.js',
+        'lib/jquery-ui-1.12.1.js',
+        'addcss.js',
+        'ui.js',
+        'revealjsctrl.js',
+        'revealjsctrl.js',
+        'websocket.js'
+      ];
+
+      // Add all Javascript files to the page
+      jsFiles.forEach(function(file) {
         chrome.tabs.executeScript({
-          code: 'var revealjsInjected = true;'
+          file: file
         });
-
-        let jsFiles = [
-          'lib/jquery-3.3.1.js',
-          'lib/jquery-ui-1.12.1.js',
-          'addcss.js',
-          'ui.js',
-          'revealjsctrl.js',
-          'revealjsctrl.js',
-          'websocket.js'
-        ];
-
-        // Add all Javascript files to the page
-        jsFiles.forEach(function(file) {
-          chrome.tabs.executeScript({ file: file });
-        });
-      }
-    });
+      });
+    }
   });
 });
